67a1e0f968c200ac56e4fa788499757ad3c248c3
Jonathan Windridge <jonathan.windridge@gmail.com>
2014-11-23 00:06:59 +0000

    Move response from console to browser window

--- apiInterface.js
+++ apiInterface.js
@@ -1,16 +1,35 @@
 var MSF = require("./MSF");
 var fs = require('fs');
+//API Credentials
 var username = "eikonstudent33@thomsonreuters.com";
 var password = "Secret123";
 
 var msf = new MSF(true);
 
+//Variables representing start & end dates for the financial mention query
 var start = "2014-11-21T00:00:00";
 var end = "2014-11-21T00:59:59";
+
+//Variable to aggregate headline queries into
 var newsArr = [];
 
-var prevLastArticleDate = "";
+//Variable representing response object (can make calls to this to display data)
+var responseObject;
 
+/*
+ * Variable to keep track of the last story date 
+ * (to work out when we have successfully downloaded all stories for a given period)
+ */
+var prevlastArticleTimestamp = "";
+
+//Function to display data in browser (as opposed to returning to console)
+function returnData(payload) {
+    responseObject.simpleText(200, JSON.stringify(payload));
+}
+
+/* Recursively called function to aggregate blocks of 20 news articles
+ * into a list representing all for the period (initially called by getHeadlineData() )
+ */
 function makeNewsRequest(){
     msf.getData(
         {
@@ -31,30 +50,46 @@ function makeNewsRequest(){
 
         }, function (error, response) {
             if (!error) {
-                for (var i = 0; i < response.Headlines.length; i++) {
-                    newsArr.push(response.Headlines[i]);
-                }
-                var lastArticleDate = response.Headlines[response.Headlines.length - 1].LastUpdateDateTime;
-                console.log(lastArticleDate);
-                if (lastArticleDate != prevLastArticleDate) {
-                    end = lastArticleDate;
-                    prevLastArticleDate = lastArticleDate
+
+                //Find the timestamp of the last article in the group of 20 
+                var lastArticleTimestamp = response.Headlines[response.Headlines.length - 1].LastUpdateDateTime;
+                
+                //Give some indication of progress on console
+                console.log(lastArticleTimestamp);
+                
+                //Check whether we have re-downloaded the same articles as before
+                if (lastArticleTimestamp != prevlastArticleTimestamp) {
+                    
+                    //Push each of the articles up into the newsArr array
+                    for (var i = 0; i < response.Headlines.length; i++) {
+                        newsArr.push(response.Headlines[i]);
+                    }
+
+                    end = lastArticleTimestamp;
+                    prevlastArticleTimestamp = lastArticleTimestamp
                     makeNewsRequest();
-                } else {
+
+                } else { //We have finished aggregating the news articles
                     //console.log(newsArr.length);
+                    
+                    //Import the mentionCounter module
                     var mentionCounter = require('./mentionCount.js');
-
+                    
+                    /*Create an array of 
+                     * [{ "ticker":"GPRO.O", "mentions":2 },......]
+                     * representing the number of times each ticker is found in the news article list
+                     */
                     var mentionCount = mentionCounter.count(newsArr);
-
+                    
+                    //Filter out the noise of companies mentioned only once or twice
                     var mentionTop = mentionCount.filter(function (el) {
                         return el.mentions >= 3;
                     });
-
+                    
+                    // Extract the list of tickers from the more-often mentioned companies (>=3)
                     var tickers = mentionTop.map(function (node) { return node.ticker; });
-                    
-                    
 
-                    console.log(tickers);
+                    returnData(tickers);
                 }
             }
         }
@@ -63,9 +98,11 @@ function makeNewsRequest(){
 }
 
 
-exports.getHeadlineData = function () {
+exports.getHeadlineData = function (func,respObject) {
     msf.login(username, password, function (error) {
         if (!error) {
+            callbackFunc = func;
+            responseObject = respObject;
             makeNewsRequest();
         }
     });
--- server.js
+++ server.js
@@ -1,20 +1,13 @@
 ﻿var server = require('./node_modules/node-router/lib/node-router.js').getServer();
 
-server.get("/", function (request, response) {
-    
-    function isBigEnough(element) {
-        return element >= 10;
-    }
-   
-    
-    //var ret = mentionCounter.count(input);
+server.get("/", function (request, response){
     response.simpleText(200, "Hello World!");
 });
 
 server.get("/test", function (request, response) {
-    response.simpleText(200, "Test response");
+    //response.simpleText(200, "Test response");
     var apiInterface = require('./apiInterface.js');
-    apiInterface.getHeadlineData();
+    apiInterface.getHeadlineData(response);
 })
 
 server.listen(8000, "localhost");
